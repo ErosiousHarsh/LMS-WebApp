de975c45b9479ed402a8fc9074ceec69126e571d
ErosiousHarsh <erosiousharsh2@gmail.com>
2021-03-01 18:21:55 +0530

    testingMainBranch

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php require "css/bootstrap.php"; ?>
+    </head>
+
+    <title>
+        LMS-Wwb    
+    </title>
+
+    <body>
+        <?php include "header.php"; ?>
+
+        <?php require "js/js.php"; ?>
+    </body>
+</html>
