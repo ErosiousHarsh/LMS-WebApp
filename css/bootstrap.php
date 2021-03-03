79120bd014b78a04ea35109c7a8d0ddc9d33e329
ErosiousHarsh <erosiousharsh2@gmail.com>
2021-03-03 23:03:43 +0530

    possbile-EOL

--- css/bootstrap.php
+++ css/bootstrap.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 <link rel="stylesheet" href="css/stylesheet.css">
--- index.php
+++ index.php
@@ -9,17 +9,38 @@
     </title>
 
     <body>
-        <section id="fullSection">
-            <div id="Top">
+        <div id="fullSection">
+            <div id="top">
                 <p class="lead" id="topText">Welcome to LMS-Web</p>
                 <div id="imgTop"></div>
             </div>
             <div id="divLogin">
-                <p>Test</p>
+                <form>
+                    <div class="form-group form-floating mb-3 has-feedback">
+                        <i class="email fas fa-at"></i>
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <input type="password" class="form-control" id="inputPassword3">
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-10 offset-sm-2">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="gridCheck1">
+                            <label class="form-check-label" for="gridCheck1">
+                            Remember Me
+                            </label>
+                        </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
             </div>
             <div id="imgBottom"> 
             </div>
-        </section>
+        </div>
         <?php require "js/js.php"; ?>
     </body>
 </html>
