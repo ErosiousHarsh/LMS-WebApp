98e84a59e1722f1ab84bf313dd161dc8ef0bdd74
ErosiousHarsh <erosiousharsh2@gmail.com>
2021-03-04 00:31:21 +0530

    FullOverhaul

--- index.php
+++ index.php
@@ -9,38 +9,27 @@
     </title>
 
     <body>
-        <div id="fullSection">
-            <div id="top">
-                <p class="lead" id="topText">Welcome to LMS-Web</p>
-                <div id="imgTop"></div>
-            </div>
-            <div id="divLogin">
-                <form>
-                    <div class="form-group form-floating mb-3 has-feedback">
-                        <i class="email fas fa-at"></i>
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1">@</span>
-                        <input type="password" class="form-control" id="inputPassword3">
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-10 offset-sm-2">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck1">
-                            <label class="form-check-label" for="gridCheck1">
-                            Remember Me
+        <section class="whole">
+            <div class="front">
+                <img class="backImg" src="images/back.png"/>
+                <div class="loginForm">
+                    <p class="lead intro">Welcome To LMS</p>
+                    <p class="lead text-white fw-bold fs-3">Login to continue</p>
+                    <!-- <form class="card card-block bg-faded">
+                        <div class="form-group input-group">
+                            <span class="input-group-addon">@</span>
+                            <label class="has-float-label">
+                                <input class="form-control" type="email" placeholder="email@example.com"/>
+                                <span>Email</span>
                             </label>
                         </div>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </form>
+                    </form> -->
+                    <form>
+                        <input type="email" placeholder="emailHere">
+                    </form>
+                </div>
             </div>
-            <div id="imgBottom"> 
-            </div>
-        </div>
+        </section>
         <?php require "js/js.php"; ?>
     </body>
 </html>
