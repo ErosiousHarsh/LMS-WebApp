cfa39703a1f8a735b5ee3c37538fd390094c78fe
ErosiousHarsh <erosiousharsh2@gmail.com>
2021-03-04 15:35:48 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -26,6 +26,20 @@
                             <input id="icon_password" type="password" class="validate bold">
                             <label for="icon_password" class="bold icon-white">Password</label>
                         </div>
+                        <div class="row mt-4">
+                            <p>
+                                <label>
+                                    <input type="checkbox" class="filled-in" checked="checked" />
+                                    <span class="text-white bold">Remember Me</span>
+                                </label>
+                                <label>
+                                    <a class="# mr-auto" href="#">Forgot password?</a>
+                                </label>
+                            </p>
+                            <button class="mt-4 btn waves-effect waves-light loginBtn" type="submit" name="action">Login
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
